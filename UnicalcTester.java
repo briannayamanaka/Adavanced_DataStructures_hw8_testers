fc756a4477e77027c2f7ac01f8fd69704444872c
imalight <jessicay1007@gmail.com>
2015-05-26 08:18:17 -0700

    Create UnicalcTester.java

---
+++ UnicalcTester.java
@@ -0,0 +1,68 @@
+//XXX change package
+package hw8solutions;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+/**
+ * Junit test class for Unicalc class.
+ */
+public class UnicalcTester {
+
+  /**
+   * Setup the tests and initialize class variables.
+   */
+  @Before
+  public void setUp() {
+  }
+
+  /**
+   *
+   */
+  @Test
+  public void testS() {
+  }
+
+  /**
+   *
+   */
+  @Test
+  public void testL() {
+  }
+
+  /**
+   *
+   */
+  @Test
+  public void testE() {
+  }
+
+  /**
+   *
+   */
+  @Test
+  public void testP() {
+  }
+
+  /**
+   *
+   */
+  @Test
+  public void testK() {
+  }
+
+  /**
+   *
+   */
+  @Test
+  public void testQ() {
+  }
+
+  /**
+   *
+   */
+  @Test
+  public void testR() {
+  }
+}
